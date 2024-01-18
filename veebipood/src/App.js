c2dbe3d31883e28d41a95d6fddf3f53f91c26293
Mihkel Vaher <mihkel.vaher@industry62.com>
2024-01-18 12:31:37 +0200

    uus reacti rakendus

---
+++ veebipood/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg'; väljakommenteerimine on juhul kui on kahju koodi kustutada
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="https://upload.wikimedia.org/wikipedia/en/9/99/Nobe_GT100.jpg" className="pilt" alt="Logo" />
+      <button className="nupp">Nupp</button>
+      <div>Tekst</div>
+      {/* <p>p</p>
+      <h1>P</h1>
+      <h2>P2</h2>
+      <h3>p3</h3> */}
+    </div>
+  );
+}
+
+export default App;
